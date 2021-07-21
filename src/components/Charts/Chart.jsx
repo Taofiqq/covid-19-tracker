dde0b1abb121368c93ce8d88a4db3f0a3bd40b72
User <taofiqaiyelabegan45@gmail.com>
2021-07-21 15:45:00 +0800

    first commit

---
+++ src/components/Charts/Chart.jsx
@@ -0,0 +1,12 @@
+import React , {useState, useEffects} from 'react'
+
+const Chart = () => {
+    const [dailyData, setDailyData] = useState({});
+    return (
+        <div>
+           Chart 
+        </div>
+    )
+}
+
+export default Chart
