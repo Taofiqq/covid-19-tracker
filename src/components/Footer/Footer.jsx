1ecb10bd45c87c7a30023276b0100c6929b5350f
User <taofiqaiyelabegan45@gmail.com>
2021-07-22 00:51:28 +0800

    added more styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import styles from './Footer.module.css'
+import { FaReact, FaArrowRight } from 'react-icons/fa';
 
 const Footer = () => {
     return (
         <div className={styles.footer}>
           <p> &copy; 2021 TaofiqCodes. All right reserved. </p>
+          <p> Build with Reactjs <FaReact /> and MaterialUI. </p>
+          <p><a href="https://covid19.mathdro.id/api">Get the API Link <FaArrowRight /></a></p>
         </div>
     )
 }
